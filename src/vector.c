12eef5a25b4640ec7cf4d3318c2907ef02e4db6b
Jan Lesiak <118604758+sniadysmok@users.noreply.github.com>
2025-05-11 17:56:18 +0200

    Update vector.c

--- src/vector.c
+++ src/vector.c
@@ -49,38 +49,3 @@ int contains(pos_int elem, pos_int elem2, const vector_pos vect, const vector_po
     }
     return 0;
 }
-// #include "../include/vector.h"
-// #include <stdlib.h>
-// #include <stdio.h>
-// void init(  vector_pos * const vect){
-//     vect->rel_size = 1;
-//     vect->size = 0;
-//   //  printf("\nmemory allocated!\n");
-//     vect->dat = malloc(sizeof(pos_int) * 1);
-// }
-// void push_back(pos_int elem, vector_pos * const vect){
-//     if(vect == NULL){return;}
-//     if(vect->size++ == vect->rel_size){
-//         vect->rel_size = vect->rel_size*2;
-//         //  printf("stinky\n");
-//         vect->dat = realloc(vect->dat, vect->rel_size * sizeof(pos_int));
-//     }
-//     vect->dat[vect->size-1] = elem;
-// }
-// void clear(vector_pos * const vect){
-//     if(vect->dat != 0){
-//        // printf("\nmemory freed!\n");
-//         free(vect->dat); // fuck
-//         vect->dat = 0;
-//     }
-//     vect->size = -1;
-//     vect->rel_size = -1;
-// }
-// int contains(pos_int elem, pos_int elem2, const vector_pos vect, const vector_pos vect2){
-//     for(int i = 0; i < vect.size; ++i){
-//         if(vect.dat[i].x == elem.x && vect.dat[i].y == elem.y && vect2.dat[i].x == elem2.x && vect2.dat[i].y == elem2.y){
-//             return 1;
-//         }
-//     }
-//     return 0;
-// }
